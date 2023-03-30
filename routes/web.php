19a8135fd1a4dac4ea54262ae4fddf7432d4b63b
nada <nada.mohamed@extech.com>
2023-03-30 21:03:12 +0200

    admin login

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,36 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => '', 'namespace' => 'App\Http\Controllers\Admin'], function () {
+    Route::get('login', function () {
+        return view('admins.pages.auth.login');
+    });
+
+    Route::post('login', 'AuthController@login')
+        ->name('login');
+
+    Route::group(['middleware' => 'auth:admin'], function () {
+
+        // logout
+        Route::get('logout', 'AuthController@logout')->name('logout');
+
+        /**
+         * Subscribers Module Routes
+         */
+        Route::resource('subscribers', 'SubscriberController')->except(['show']);
+
+        Route::prefix('subscribers')->group(function () {
+            Route:: as ('subscribers.')->group(function () {
+                Route::get('data', 'SubscriberController@data')->name('data');
+            });
+        });
+
+    });
+
 });
